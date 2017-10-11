4a9d078bcff4d014033a7f9bc9e8186d0d0a19a4
billwang1996 <31968973+billwang1996@users.noreply.github.com>
2017-10-10 22:47:33 -0400

    Removed project in case

--- 2nd_year/csc207/src/model/FileType.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-/**
- * FileType that can be used to specifying the type for a FileNode. IMAGE for a
- * image file. FILE for a non-image file. Directory for a directory.
- */
-public enum FileType {
-	IMAGE, FILE, DIRECTORY
-}
